201843963182aa1357aad4aed0ef9effd8013bc2
WalidEll <walid.elalaouy@gmail.com>
2026-01-29 07:37:04 -0800

    Fix missing PathVariable import

--- src/main/java/com/wellycloud/portfolio/api/PortfolioController.java
+++ src/main/java/com/wellycloud/portfolio/api/PortfolioController.java
@@ -5,6 +5,7 @@ import com.wellycloud.portfolio.repo.InstrumentRepo;
 import com.wellycloud.portfolio.service.PortfolioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
